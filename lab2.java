12fa312619897ef3b102743961b0022329d9b68c
NAVI-SL <navithanavindul@gmail.com>
2026-04-28 16:25:10 +0530

    Comment out code in lab2.java for Q1 exercise

--- lab2.java
+++ lab2.java
@@ -1,47 +1,47 @@
-// Q1
-import java.util.Scanner;
+// // Q1
+// import java.util.Scanner;
 
-public class lab2  {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+// public class lab2  {
+//     public static void main(String[] args){
+//         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the first Mark: ");
-        int q1 = scanner.nextInt();
+//         System.out.print("Enter the first Mark: ");
+//         int q1 = scanner.nextInt();
 
-        System.out.print("Enter the second Mark: ");
-        int q2 = scanner.nextInt();
+//         System.out.print("Enter the second Mark: ");
+//         int q2 = scanner.nextInt();
 
-        System.out.print("Enter the second Mark: ");
-        int q3 = scanner.nextInt();
+//         System.out.print("Enter the second Mark: ");
+//         int q3 = scanner.nextInt();
 
-        System.out.print("Enter the fourth Mark: ");
-        int q4 = scanner.nextInt();
+//         System.out.print("Enter the fourth Mark: ");
+//         int q4 = scanner.nextInt();
         
-        int total = q1 + q2 + q3 + q4;
-        double average = total / 4.0;
+//         int total = q1 + q2 + q3 + q4;
+//         double average = total / 4.0;
 
-        if(total >= 80){
-            System.out.println('A');
-        }
-        else if(total >= 65){
-            System.out.println('B');
-        }
-        else if (total >= 50){
-            System.out.println('C');
-        }
-        else if(total < 50){
-            System.out.println('F');
-        }
-        else{
-            System.out.println("Invalid Marks");
-        }
+//         if(total >= 80){
+//             System.out.println('A');
+//         }
+//         else if(total >= 65){
+//             System.out.println('B');
+//         }
+//         else if (total >= 50){
+//             System.out.println('C');
+//         }
+//         else if(total < 50){
+//             System.out.println('F');
+//         }
+//         else{
+//             System.out.println("Invalid Marks");
+//         }
 
-        System.out.println("Total: " + total);
-        System.out.println("Average" + average);
+//         System.out.println("Total: " + total);
+//         System.out.println("Average" + average);
 
-        scanner.close();
-    }
-}
+//         scanner.close();
+//     }
+// }
 
 // // Q2
 // public class lab2 {
