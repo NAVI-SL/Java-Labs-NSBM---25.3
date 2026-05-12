15a7bd7ca1eb89cc6e395889641ce4b84147f30c
NAVI-SL <navithanavindul@gmail.com>
2026-05-12 15:38:35 +0530

    fsd

---
+++ lab3.java
@@ -0,0 +1,15 @@
+public class lab3 {
+    int id;
+    String name;
+
+    lab3() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        lab3 c1 = new lab3();
+
+        System.out.println(c1.id);
+        System.out.println(c1.name);
+    }
+}
