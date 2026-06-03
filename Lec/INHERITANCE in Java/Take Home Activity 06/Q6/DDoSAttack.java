dcbf2bb82ddf296a6d9bbed642eca41f3a796327
NAVI-SL <navithanavindul@gmail.com>
2026-06-03 16:19:54 +0530

    Remove DDoSAttack, MalwareAttack, PhishingAttack, and Threat classes

--- Lec/INHERITANCE in Java/Take Home Activity 06/Q6/DDoSAttack.java
+++
@@ -1,9 +0,0 @@
-package Q6;
-
-public class DDoSAttack extends Thread {
-    int packetCount;
-
-    public void mitigate() {
-        System.out.println("Mitigating DDoS from " );
-    } 
-}
--- Lec/INHERITANCE in Java/Take Home Activity 06/Q6/PhishingAttack.java
+++
@@ -1,9 +0,0 @@
-package Q6;
-
-public class PhishingAttack extends Thread {
-    String targetEmail;
-
-    public void blockEmail() {
-        System.out.println("Blocking phishing email targeting: " + targetEmail);
-    }
-}
