d1efa015977ccbc452114233f92bdfcad201bb43
Navitha Navindul <navithanavindul@gmail.com>
2026-06-06 22:11:15 +0530

    Add Main class to Q2 package for testing Printer functionality

---
+++ Take Home Activity/Take Home Activity 07/Q2/Main.java
@@ -0,0 +1,12 @@
+package Q2;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer p1 = new Printer();
+
+        p1.printData("Hello");
+        p1.printData(18);
+        p1.printData("Navitha,18");
+        p1.printData(true);
+    }
+}
