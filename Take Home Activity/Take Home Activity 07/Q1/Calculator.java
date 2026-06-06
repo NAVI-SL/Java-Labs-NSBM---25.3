a06fb33ad4d57de162d4c04c35f95bb1207dc7d1
Navitha Navindul <navithanavindul@gmail.com>
2026-06-06 21:31:06 +0530

    Refactor Calculator methods to return results and add Main class for testing

--- Take Home Activity/Take Home Activity 07/Q1/Calculator.java
+++ Take Home Activity/Take Home Activity 07/Q1/Calculator.java
@@ -1,17 +1,26 @@
 public class Calculator {
 
-    void add(int a, int b) {
+    int add(int a, int b) {
         int result = a + b;
         System.out.println("Int sum: " + result);
+        return result;
     }
 
-    void add(double a, double b) {
+    double add(double a, double b) {
         double result = a + b;
         System.out.println("Double sum: " + result);
+        return result;
     }
 
-    void add(int a, int b, int c) {
+    int add(int a, int b, int c) {
         int result = a + b + c;
         System.out.println("Three-int sum: " + result);
+        return result;
+    }
+
+    String add(String a, String b) {
+        String result = a + b;
+        System.out.println("String join: " + result);
+        return result;
     }
 }
---
+++ Take Home Activity/Take Home Activity 07/Q1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        calc.add(10, 20);
+        calc.add(5.5, 4.5);
+        calc.add(10, 20, 30);
+        calc.add("Hello ", "Java");
+
+        
+        
+    }
+}
